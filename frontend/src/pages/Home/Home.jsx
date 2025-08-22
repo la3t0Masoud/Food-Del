d2d8fbbaccb1492bfd30f387a0d9cadd022f8fbe
la3t0Masoud <la3t.masoud11@gmail.com>
2025-08-22 16:17:01 +0330

    Eplore part + counter of each item added

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
 
 const Home = () => {
 
@@ -10,7 +11,9 @@ const Home = () => {
   return (
     <div>
       <Header/>
+      <hr />
       <ExploreMenu category={category} setCategory={setCategory}/>
+      <FoodDisplay category={category}/>
     </div>
   )
 }
