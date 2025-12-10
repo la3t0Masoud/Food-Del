9229e97e06efc92acd6443fbc7678ccdcb6d8e90
la3t0Masoud <la3t.masoud11@gmail.com>
2025-12-10 16:55:03 +0330

    some console bugs fixed

--- frontend/src/pages/SearchBar/SerachBar.jsx
+++ frontend/src/pages/SearchBar/SerachBar.jsx
@@ -1,53 +1,53 @@
-import React,{useContext,useState,useEffect} from 'react'
-import Navbar from '../../components/Navbar/Navbar'
-import FoodItem from '../../components/FoodItem/FoodItem'
-import { StoreContext } from '../../context/StoreContext'
-import './SearchBar.css'
+import React, { useContext, useState, useEffect } from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import FoodItem from "../../components/FoodItem/FoodItem";
+import { StoreContext } from "../../context/StoreContext";
+import "./SearchBar.css";
 
 const SerachBar = () => {
-    
-    const [SearchingFood, setSearchingFood] = useState('');
+  const [SearchingFood, setSearchingFood] = useState("");
 
-    const{food_list} = useContext(StoreContext);
+  const { food_list } = useContext(StoreContext);
 
-    const handleSearch = async(e) =>{
-            e.preventDefault()
-            console.log(e.target.querySelector('.search-input').value);
-           setSearchingFood( e.target.querySelector('.search-input').value);
-        };
-    
-    
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    setSearchingFood(e.target.querySelector(".search-input").value);
+  };
+
   return (
     <>
-    <div className='home'>
-        <form onSubmit={handleSearch} className='search-form'>
-            <input 
-                type="text" 
-                placeholder='Search for Food ...' 
-                className="search-input" 
-            />
-            <button type='submit' className="search-button">Search</button>
+      <div className="home">
+        <form onSubmit={handleSearch} className="search-form">
+          <input
+            type="text"
+            placeholder="Search for Food ..."
+            className="search-input"
+          />
+          <button type="submit" className="search-button">
+            Search
+          </button>
         </form>
-    </div>
-    <div className='food-display' id='food-display'>
-
+      </div>
+      <div className="food-display" id="food-display">
         <div className="food-display-list">
-            {food_list.filter(item => 
-                item.name.toLowerCase().startsWith(SearchingFood.toLowerCase())
-            ).map((item, index) => (
-                <FoodItem 
-                    key={index} 
-                    id={item._id} 
-                    name={item.name} 
-                    description={item.description} 
-                    price={item.price} 
-                    image={item.image} 
-                />
+          {food_list
+            .filter((item) =>
+              item.name.toLowerCase().startsWith(SearchingFood.toLowerCase())
+            )
+            .map((item, index) => (
+              <FoodItem
+                key={index}
+                id={item._id}
+                name={item.name}
+                description={item.description}
+                price={item.price}
+                image={item.image}
+              />
             ))}
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default SerachBar
+export default SerachBar;
